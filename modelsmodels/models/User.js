4d2a507303b21fef36204c813917599067cba302
Escobar0123 <Escobarxchange73@gmail.com>
2026-09-10 18:34:53 +0100

    Delete modelsmodels/models directory

--- modelsmodels/models/User.js
+++
@@ -1,3 +0,0 @@
-const mongoose=require('mongoose');
-const s=new mongoose.Schema({username:String,email:{type:String,unique:true},password:String,balance:{type:Number,default:0},totalDeposits:{type:Number,default:0},totalWithdrawals:{type:Number,default:0},totalBets:{type:Number,default:0},isAdmin:Boolean,createdAt:{type:Date,default:Date.now}});
-module.exports=mongoose.model('User',s);
